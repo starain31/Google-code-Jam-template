31f5e4430bbda959f5a9f4ad806093a1b747e6cf
Nazmus Sakib <nsakib.cse@gmail.com>
2016-04-08 02:19:42 +0600

    shuru korlam

---
+++ a.cpp
@@ -0,0 +1,24 @@
+#include "iostream"
+#include "algorithm"
+
+using namespace std;
+
+typedef long long ll;
+
+const size_t LIMIT = 800+5;
+int n;
+
+bool init(/* arguments */) {
+	/* code */
+	return 0;
+}
+
+bool read_input(/* arguments */) {
+	/* code */
+	return true;
+}
+
+string solve(/* arguments */) {
+	/* code */
+	return to_string(0);
+}
